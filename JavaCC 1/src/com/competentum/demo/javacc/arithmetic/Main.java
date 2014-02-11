264cc6b07dd7442bf7d024de952b96d3f19cc9a0
Obidin <obidin@phystech.edu>
2014-02-11 23:50:02 +0400

    Add first demo

---
+++ JavaCC 1/src/com/competentum/demo/javacc/arithmetic/Main.java
@@ -0,0 +1,15 @@
+package com.competentum.demo.javacc.arithmetic;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+public class Main {
+	public static void main(String[] args) throws UnsupportedEncodingException, ParseException {
+		String expression = "1/2/3";
+		InputStream stream = new ByteArrayInputStream(expression.getBytes("UTF-8"));
+		Arithmetic parser = new Arithmetic(stream);
+		parser.parse();
+		
+	}
+}
